977fdab38cbe6b084d70a22660416dff552b5540
schulke-214 <schulkemaximilian@gmail.com>
2019-06-22 13:11:28 +0200

    refactor: create a wrapper around animejs and scss variable imports

---
+++ helper/animation.js
@@ -0,0 +1,11 @@
+import anime from 'animejs';
+import variables from 'styles/helper/vars.scss';
+
+export const {
+    ease,
+} = variables;
+
+export const duration = 750;
+
+// export const duration = CSSDurationStringToJSNumber(variables.duration);
+export default anime;
