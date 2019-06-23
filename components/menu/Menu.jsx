205dc969099f6ec44d45980168debf0552fe2053
schulke-214 <schulkemaximilian@gmail.com>
2019-06-23 14:41:37 +0200

    feat(Menu): unmount the menu when its unused

--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 import { ease, duration } from 'helper/animation';
 
-import Logo from 'components/logo/Logo';
+import Logo from 'components/stateless/logo/Logo';
 import HugeLink from 'components/links/huge-link/HugeLink';
 import SmallLink from 'components/links/small-link/SmallLink';
 
@@ -10,89 +10,122 @@ const MenuTopic = ({ children, className }) => (
 	<h2 className={'menu__topic' + ' ' + className}>{children}</h2>
 );
 
+const Place = ({ title, children }) => (
+	<div className='menu__place col-12'>
+		<h4 className='typo-flags --primary --letter-spacing-wide --medium'>{title}</h4>
+		<p className='typo-flags --letter-spacing-wide --light'>{children}</p>
+	</div>
+);
+
 class Menu extends React.Component {
 	menu = React.createRef();
 	backdrop = React.createRef();
 
-	componentDidMount() {
-		this.tl = new TimelineLite({ paused: true });
-
-		this.tl
-			.to(
-				this.menu.current,
-				duration,
-				{
-					y: 0,
-					ease
-				},
-				0
-			)
-			.to(
-				this.backdrop.current,
-				duration,
-				{
-					backgroundColor: 'rgba(0,0,0,0.1)',
-					pointerEvents: 'all',
-					ease
-				},
-				0
-			);
-	}
+	state = {
+		mounted: false
+	};
 
 	componentDidUpdate(prevProps) {
 		if (prevProps.open == this.props.open) return;
 
 		if (this.props.open) {
-			this.tl.play();
+			this.setState({ mounted: true }, this.open);
 		} else {
-			this.tl.reverse();
+			this.close().then(() => this.setState({ mounted: false }));
 		}
 	}
 
+	open = () => {
+		TweenLite.to(this.menu.current, duration, {
+			ease,
+			y: 0
+		});
+
+		TweenLite.to(this.backdrop.current, duration, {
+			ease,
+			backgroundColor: 'rgba(0,0,0,0.1)',
+			pointerEvents: 'all'
+		});
+	};
+
+	close = () =>
+		new Promise((resolve, reject) => {
+			TweenLite.to(this.menu.current, duration, {
+				ease,
+				y: '-100%'
+			});
+
+			TweenLite.to(this.backdrop.current, duration, {
+				ease,
+				backgroundColor: 'rgba(0,0,0,0)',
+				pointerEvents: 'none',
+				onComplete: resolve
+			});
+		});
+
+	unmount = () => {
+		this.setState({ mounted: false });
+	};
+
 	render() {
-		//className={`menu ${this.props.open ? 'menu--open' : ''}`}> hi im the menu
+		if (!this.state.mounted) return <></>;
+
 		return (
 			<>
-				<div ref={this.menu} className='menu'>
-					<div className='container'>
-						<div className='row'>
-							<div className='col-12 col-tablet-small-3 offset-tablet-small-1 display__flex--flags --col'>
-								<div className='row'>
-									<div
-										className='col-12 display__flex display__flex--flags --align-items-center'
-										style={{ height: '40px' }}>
-										<Logo />
+				<div className='menu' ref={this.menu}>
+					<div className='menu__content'>
+						<div className='container'>
+							<div className='row'>
+								<div className='col-12 col-tablet-small-3 offset-tablet-small-1 display__flex--flags --col'>
+									<div className='row'>
+										<div
+											className='col-12 display__flex display__flex--flags --align-items-center'
+											style={{ height: '40px' }}>
+											<Logo />
+										</div>
+									</div>
+									<div className='menu__content-col row display__none display-tablet-small__flex '>
+										<div className='col'>
+											<SmallLink href='/archiv'>archiv</SmallLink>
+											<SmallLink href='/newsletter'>newsletter</SmallLink>
+											<SmallLink href='/blog'>blog</SmallLink>
+											<SmallLink href='/presse'>presse</SmallLink>
+											<SmallLink href='/abc'>instagram</SmallLink>
+										</div>
 									</div>
 								</div>
-								<div className='menu__content-col row display__none display-tablet-small__flex '>
-									<div className='col'>
-										<SmallLink>archiv</SmallLink>
-										<SmallLink>newsletter</SmallLink>
-										<SmallLink>blog</SmallLink>
-										<SmallLink>presse</SmallLink>
-										<SmallLink>instagram</SmallLink>
+								<div className='col-12 col-tablet-small-4 display__flex display__flex--flags --col'>
+									<MenuTopic className='display__none display-tablet-small__block'>
+										Inhalte
+									</MenuTopic>
+									<div className='menu__content-col row'>
+										<div className='col'>
+											<HugeLink href='/work'>Arbeiten</HugeLink>
+											<HugeLink href='/about'>Über Uns</HugeLink>
+											<HugeLink href='/news'>Neuigkeiten</HugeLink>
+											<HugeLink href='/jobs'>Stellenangebote</HugeLink>
+											<HugeLink href='/contact'>Kontakt</HugeLink>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div className='col-12 col-tablet-small-4 display__flex display__flex--flags --col'>
-								<MenuTopic className='display__none display-tablet-small__block'>
-									Inhalte
-								</MenuTopic>
-								<div className='menu__content-col row'>
-									<div className='col'>
-										<HugeLink>Arbeiten</HugeLink>
-										<HugeLink>Über Uns</HugeLink>
-										<HugeLink>Neuigkeiten</HugeLink>
-										<HugeLink>Stellenangebote</HugeLink>
-										<HugeLink>Kontakt</HugeLink>
+								<div className='col-tablet-small-4 display__none display-tablet-small__flex display__flex--flags --col'>
+									<MenuTopic className='display__none display-tablet-small__block'>
+										Räumlichkeiten
+									</MenuTopic>
+									<div className='menu__content-col row'>
+										<Place title='Hauptbüro'>
+											Wertherstraße 310 <br />
+											33619 Bielefeld <br />
+											Deutschland <br />
+										</Place>
+
+										<Place title='Werktstatt'>
+											Höfeweg XYZ <br />
+											33624 Bielefeld <br />
+											Deutschland <br />
+										</Place>
 									</div>
 								</div>
-							</div>
-							<div className='col-tablet-small-4 display__none display-tablet-small__flex display__flex--flags --col'>
-								<MenuTopic className='display__none display-tablet-small__block'>
-									Räumlichkeiten
-								</MenuTopic>
-								<div className='menu__content-col row' />
 							</div>
 						</div>
 					</div>
