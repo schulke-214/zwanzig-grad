fa3c049d6bb7617cf362faf739472b9e528476c3
schulke-214 <schulkemaximilian@gmail.com>
2019-06-23 14:35:29 +0200

    feat: create a footer

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.scss';
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<footer className='container'>
+				<h1>footer</h1>
+				<h2>Hi</h2>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
