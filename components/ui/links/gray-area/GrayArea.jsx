724524ac32e906372c8f248e342045ba54c80f46
schulke-214 <schulkemaximilian@gmail.com>
2019-06-25 22:19:29 +0200

    feat: add a grayarea

---
+++ components/ui/links/gray-area/GrayArea.jsx
@@ -0,0 +1,40 @@
+import { smoothEase, duration } from 'helper/animation';
+
+import './GrayArea.scss';
+
+export class GrayArea extends React.Component {
+	span = React.createRef();
+
+	componentDidMount() {
+		this.tl = new TimelineMax({ paused: true })
+			.set(this.span.current, {
+				transformOrigin: 'left'
+			})
+			.to(this.span.current, duration / 4, {
+				ease: smoothEase,
+				scaleX: 0.66
+			})
+			.addLabel('half')
+			.to(this.span.current, duration / 4, {
+				ease: smoothEase,
+				scaleX: 1
+			})
+			.to(this.span.current, duration / 4, {
+				ease: smoothEase,
+				transformOrigin: 'right',
+				scaleX: 0
+			});
+	}
+
+	show = () => {
+		this.tl.tweenFromTo(0, 'half');
+	};
+
+	hide = () => {
+		this.tl.play();
+	};
+
+	render() {
+		return <span ref={this.span} className='link__gray-area' style={this.props.style} />;
+	}
+}
