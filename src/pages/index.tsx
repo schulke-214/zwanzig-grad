7760630a3a425b107e3a88a0dbbd8f816404f2f0
schulke-214 <hello@maximilianschulke.com>
2020-07-25 17:22:55 +0200

    add a index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import React, {FunctionComponent} from 'react';
 
+import Layout from 'layouts/default';
+
 
 interface HomeProps {}
 
 const Home: FunctionComponent<HomeProps> = (props: HomeProps) => (
-	<div>
-		<h1>home</h1>	
-	</div>
+	<Layout>
+		<h1>home</h1>
+		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, maxime ad. Blanditiis, alias hic corporis eligendi quis quia quibusdam molestiae culpa suscipit ex illo quod eum quam omnis, reiciendis beatae!</p>
+		<h2>sub</h2>
+		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, maxime ad. Blanditiis, alias hic corporis eligendi quis quia quibusdam molestiae culpa suscipit ex illo quod eum quam omnis, reiciendis beatae!</p>
+		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, maxime ad. Blanditiis, alias hic corporis eligendi quis quia quibusdam molestiae culpa suscipit ex illo quod eum quam omnis, reiciendis beatae!</p>
+	</Layout>
 )
 
 export default Home;
