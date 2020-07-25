ca70fe5dc5b81e7c52d749150f95cbf523bb862b
schulke-214 <hello@maximilianschulke.com>
2020-07-25 15:45:12 +0200

    setup typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react';
+
+
+interface HomeProps {}
+
+const Home: FunctionComponent<HomeProps> = (props: HomeProps) => (
+	<div>
+		<h1>home</h1>	
+	</div>
+)
+
+export default Home;
