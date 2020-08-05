fc5a5d01894a77784afeccb0c0ef5963b46ffbf9
schulke-214 <hello@maximilianschulke.com>
2020-08-05 22:04:08 +0200

    refactor media queries to use mobile first

--- src/lib/media.ts
+++ src/lib/media.ts
@@ -1,12 +1,12 @@
 import { rem } from 'lib/polished';
 
 export const breakpoints = {
-    lg: 996,
-    md: 760,
+	lg: 1140,
+	md: 996,
     sm: 576
 };
 
 export const desktop = `@media (min-width: ${rem(breakpoints.lg)})`;
-export const tablet = `@media (max-width: ${rem(breakpoints.lg)})`;
-export const landscape = `@media (max-width: ${rem(breakpoints.md)})`;
+export const tablet = `@media (min-width: ${rem(breakpoints.md)})`;
+export const landscape = `@media (min-width: ${rem(breakpoints.sm)})`;
 export const mobile = `@media (max-width: ${rem(breakpoints.sm)})`;
