e60ef7ede2beda3a41be79e3def3000729963f2e
schulke-214 <hello@maximilianschulke.com>
2020-08-11 23:05:47 +0200

    add a homepage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,33 @@
 import React, {FunctionComponent} from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from 'layouts/default';
+import PageLayout from 'components/PageLayout';
 
 
-const Home: FunctionComponent<{}> = () => (
-	<Layout>
-		<h1>home</h1>
-		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, maxime ad. Blanditiis, alias hic corporis eligendi quis quia quibusdam molestiae culpa suscipit ex illo quod eum quam omnis, reiciendis beatae!</p>
-		<h2>sub</h2>
-		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, maxime ad. Blanditiis, alias hic corporis eligendi quis quia quibusdam molestiae culpa suscipit ex illo quod eum quam omnis, reiciendis beatae!</p>
-		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, maxime ad. Blanditiis, alias hic corporis eligendi quis quia quibusdam molestiae culpa suscipit ex illo quod eum quam omnis, reiciendis beatae!</p>
-	</Layout>
-)
+const Home: FunctionComponent<{}> = () => {
+	const data = useStaticQuery(graphql`
+		{
+			allContentfulStartseite {
+				edges {
+					node {
+						layout {
+							...Layout
+						}
+					}
+				}
+			}
+		}
+	`);
+
+	const { layout } = data.allContentfulStartseite.edges[0].node;
+
+	return (
+		<Layout>
+			<PageLayout {...layout} />
+		</Layout>
+	);
+}
+
 
 export default Home;
