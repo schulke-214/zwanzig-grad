dd0cfd273c77ee798bbc6be83150d2b2e22c4e81
schulke-214 <hello@maximilianschulke.com>
2020-08-13 22:52:49 +0200

    generate pages and thier subpages from contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,13 +3,21 @@ const path = require('path');
 exports.createPages = async ({ graphql, actions }) => {
 	const { createPage } = actions;
 	const pageTemplate = path.resolve('./src/templates/page.tsx');
+	const subpageTemplate = path.resolve('./src/templates/subpage.tsx');
 
 	const {errors, data} = await graphql(`
 		{
 			allContentfulSeite {
 				edges {
 					node {
-						slug
+						metadata {
+							slug
+						}
+						subpages {
+							metadata {
+								slug
+							}
+						}
 					}
 				}
 			}
@@ -23,14 +31,41 @@ exports.createPages = async ({ graphql, actions }) => {
 	}
 
 	const pages = data.allContentfulSeite.edges;
-
+	
 	pages.forEach(page => {
-		createPage({
-			path: `/${page.node.slug}/`,
-			component: pageTemplate,
-			context: {
-				slug: page.node.slug
-			}
-		});
+		const pageSlug = page.node.metadata.slug;
+		const isHomePage = pageSlug === null || pageSlug.trim() === '';
+
+		if (isHomePage) {
+			createPage({
+				path: `/`,
+				component: pageTemplate,
+				context: {
+					slug: null
+				}
+			});
+		} else {
+			createPage({
+				path: `/${pageSlug}/`,
+				component: pageTemplate,
+				context: {
+					slug: pageSlug
+				}
+			});
+		}
+		
+		if (!page.node.subpages) return;
+
+		const prefix = isHomePage ? '/' : `/${pageSlug}/`;
+
+		page.node.subpages.forEach(subpage => {
+			createPage({
+				path: `${prefix}${subpage.metadata.slug}/`,
+				component: subpageTemplate,
+				context: {
+					slug: subpage.metadata.slug
+				}
+			});
+		})
 	});
 }
