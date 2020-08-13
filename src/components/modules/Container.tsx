0e9b920c0370cc81b68dfb634dadb9b72fb50149
schulke-214 <hello@maximilianschulke.com>
2020-08-14 00:30:53 +0200

    style the container module

--- src/components/modules/Container.tsx
+++ src/components/modules/Container.tsx
@@ -1,27 +1,45 @@
 import React, { FunctionComponent } from 'react'
-import Text from './Text';
+import styled from 'styled-components';
 import PageLayout from 'components/PageLayout';
+import { rem } from 'lib/polished';
+import { tablet } from 'lib/media';
 
 
 type FlexDirection = 'row' | 'column';
 type ContainerDirection = 'Horizontal' | 'Vertical';
 
 interface ContainerProps {
+	className?: string;
 	type: ContainerDirection;
 	content: any;
 }
 
-const Container: FunctionComponent<ContainerProps> = ({ type, content }) => {
+const Container: FunctionComponent<ContainerProps> = ({ className, type, content }) => {
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: type === 'Horizontal' ? 'row' : type === 'Vertical' ? 'column' : undefined
-			}}
-		>
+		<div className={className}>
 			<PageLayout content={content} />
 		</div>
 	);
 }
 
-export default Container;
+export default styled(Container)`
+	display: flex;
+	flex-direction: column;
+
+	${tablet} {
+		flex-direction: ${props => props.type === 'Horizontal' ? 'row' : props.type === 'Vertical' ? 'column' : 'unset'};
+	}
+
+	> div:not(:first-child) {
+		margin-top: ${props => rem(props.theme.spacings.medium)};
+
+		${tablet} {
+			margin-top: 0;
+			margin-left: ${props => rem(props.theme.spacings.large)};
+		}
+	}
+
+	> div {
+		flex: 1 0 0;
+	}
+`;
