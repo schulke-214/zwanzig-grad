4b790d2f6c850495a598e6cc887aaa407dc52e0b
schulke-214 <hello@maximilianschulke.com>
2021-02-12 23:30:37 +0100

    add project data types and fragments

---
+++ src/data/project.ts
@@ -0,0 +1,29 @@
+import { graphql } from 'gatsby';
+
+import { CMSResponsiveImage } from 'data/cms';
+
+
+export type Project = {
+	slug: string;
+	title: string;
+	description: string
+	client: string;
+	year: number;
+	materials: string[];
+	titleImage: CMSResponsiveImage;
+}
+
+export default graphql`
+	fragment Project on ContentfulProjekt {
+		slug
+		title
+		year
+		client
+		tileImage {
+			fluid(quality: 80, , maxWidth: 720) {
+				src
+			}
+			description
+		}
+	}
+`;
