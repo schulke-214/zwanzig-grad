18110896e0b336bbb52939c7cbbf7544c3e73dfc
schulke-214 <hello@maximilianschulke.com>
2021-02-12 18:43:05 +0100

    fix the formatting of phone numbers

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,2 +1,2 @@
 export const phoneNumber = (nr: string = ''): string =>
-	`${nr.substring(0, 3)} ${nr.substring(3).match(/.{1,2}/g)?.join(' ') || ''}`
+	`${nr.substring(0, 3)} ${nr.substring(3, 6)} ${nr.substring(6).match(/.{1,2}/g)?.join(' ') || ''}`
