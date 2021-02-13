510837e3a65f65b69d47f51d4add3bc0c1ffec45
schulke-214 <hello@maximilianschulke.com>
2021-02-13 15:12:01 +0100

    add a stage module

---
+++ src/components/modules/Stage.tsx
@@ -0,0 +1,85 @@
+import React, { Fragment, FunctionComponent } from 'react'
+import styled from 'styled-components';
+
+import { LayoutModuleStage } from 'data/layout';
+
+import { rem } from 'lib/polished';
+import { tablet } from 'lib/media';
+
+
+const StageBackground = styled.img`
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: ${props => props.theme.layers.default.background};
+	display: block;
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+
+const StageContent = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 90vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: ${props => rem(props.theme.spacings.medium)};
+
+	h1 {
+		color: ${props => props.theme.colors.background};
+		font-size: 2.5rem;
+		margin: 0;
+		padding: 0;
+		border-bottom: 0;
+		text-align: center;
+
+		${tablet} {
+			font-size: 4rem;
+		}
+
+		span {
+			text-align: center;
+			display: block;
+			margin: 0 auto;
+			color: inherit;
+			font-size: 75%;
+			margin-top: ${props => rem(props.theme.spacings.small)};
+		}
+
+		em {
+			font-style: normal;
+			color: inherit;
+		}
+	}
+`;
+
+const StageSpacer = styled.div`
+	height: calc(90vh - ${props => rem(props.theme.logo.height + 2 * props.theme.spacings.medium + props.theme.spacings.large)});
+`;
+
+interface StageProps extends LayoutModuleStage {
+	className?: string;
+}
+
+const Stage: FunctionComponent<StageProps> = ({ className, staticText, connection, buzzWords, background }) => {
+	return (
+		<div className={className}>
+			<StageContent>
+				<h1>
+					{staticText}
+					<span>{connection} <em>{buzzWords[0]}</em></span>
+				</h1>
+				<StageBackground src={background.desktop.src} alt={background.title} />
+			</StageContent>
+			<StageSpacer />
+		</div>
+	);
+}
+
+export default styled(Stage)`
+`;
