ad201656f28d339b020becabe8e45a946eac1995
schulke-214 <hello@maximilianschulke.com>
2021-02-13 15:11:27 +0100

    update the module container spacings

--- src/components/generic/ModuleContainer.tsx
+++ src/components/generic/ModuleContainer.tsx
@@ -1,21 +1,15 @@
 import styled from 'styled-components';
 import { rem } from 'lib/polished';
-import { tablet, mobile } from 'lib/media';
+import { tablet } from 'lib/media';
 
 
 export default styled.div`
 	position: relative;
 	z-index: ${props => props.theme.layers.default.content};
-	margin-bottom: ${props => rem(props.theme.spacings.medium)};
-
-	${mobile} {
-		&:last-child {
-			margin-bottom: 0;
-		}
-	}
+	margin-bottom: ${props => rem(props.theme.spacings.large)};
 
 	${tablet} {
-		margin-bottom: ${props => rem(props.theme.spacings.large)};
+		margin-bottom: ${props => rem(props.theme.spacings.xlarge)};
 	}
 
 	& > div {
