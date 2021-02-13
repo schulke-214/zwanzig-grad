f83477cc51d530f99b35559cef501a5b09fb7c4c
schulke-214 <hello@maximilianschulke.com>
2021-02-13 17:03:48 +0100

    add footer content

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,19 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
-import ContactData from 'components/generic/ContactData';
+import ContactData, {ContactDataItem} from 'components/generic/ContactData';
+import NavigationItem from 'components/layout/NavigationItem';
+import { NavigationLinks } from 'components/layout/Navigation';
 
 import { rem } from 'lib/polished';
-import { tablet } from 'lib/media';
+import { tablet, landscape } from 'lib/media';
 
 
 const FooterContainer = styled.footer`
 	width: 100%;
 	height: min-content;
-	background-color: ${props => props.theme.colors.brand};
-	padding: ${props => rem(props.theme.spacings.medium)};
+	background-color: ${props => props.theme.colors.muted};
+	padding: ${props => rem(props.theme.spacings.large)};
 	
 	${tablet} {
 		padding: ${props => rem(props.theme.spacings.xlarge)} ${props => rem(props.theme.spacings.large)};;
@@ -19,8 +21,34 @@ const FooterContainer = styled.footer`
 `;
 
 const FooterContent = styled.div`
-	max-width: ${props => rem(props.theme.layout.maxWidth - 2 * props.theme.spacings.large)};
+	max-width: ${props => rem(props.theme.layout.maxWidth - 8 * props.theme.spacings.large)};
 	margin: 0 auto;
+
+	${ContactData} {
+		${landscape} {
+			display: flex;
+			flex-direction: row;
+		}
+
+		> ${ContactDataItem} {
+			${landscape} {
+				margin-bottom: 0;
+				width: 33.33%;
+			}
+
+			li {
+				margin-bottom:  ${props => rem(props.theme.spacings.small)};
+			}
+
+			${NavigationItem} {
+				font-size: unset;
+
+				&::before {
+					border-bottom-width: 1px;
+				}
+			}
+		}
+	}
 `;
 
 interface FooterProps {}
@@ -28,7 +56,12 @@ interface FooterProps {}
 const Footer: FunctionComponent<FooterProps> = ({}) => (
 	<FooterContainer>
 		<FooterContent>
-			<ContactData />
+			<ContactData>
+				<ContactDataItem>
+					<strong>Seitenübersicht</strong>
+					<NavigationLinks />
+				</ContactDataItem>
+			</ContactData>
 		</FooterContent>
 	</FooterContainer>
 );
