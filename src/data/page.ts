8b61b987df050e0d3b7187f9d849ce2821551854
schulke-214 <hello@maximilianschulke.com>
2021-02-17 00:25:31 +0100

    add page types and fragments

---
+++ src/data/page.ts
@@ -0,0 +1,54 @@
+import { graphql } from 'gatsby';
+
+import { Layout } from 'data/layout';
+
+
+export type Page = {
+	slug: string;
+	title: string;
+	metadata: PageMetaData;
+	layout: Layout;
+};
+
+export type Subpage = Page;
+
+export type PageMetaData = {
+	id: string;
+	slug: string;
+	description: string;
+	keywords: string[];
+};
+
+export const Page = graphql`
+	fragment Page on ContentfulSeite {
+		id
+		title
+		layout {
+			...Layout
+		}
+		metadata {
+			...PageMetaData
+		}
+		subpages {
+			...Subpage
+		}
+	}
+
+	fragment Subpage on ContentfulSeiteUnterseite {
+		id
+		title
+		layout {
+			...Layout
+		}
+		metadata {
+			...PageMetaData
+		}
+	}
+
+	fragment PageMetaData on ContentfulSeiteMetadaten {
+		id
+		slug
+		description
+		keywords
+	}
+`;
