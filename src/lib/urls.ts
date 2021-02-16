0864b00b24eb8cec8cccc74a5fb8ed466ce4c2eb
schulke-214 <hello@maximilianschulke.com>
2021-02-17 00:24:15 +0100

    add url resolvers

---
+++ src/lib/urls.ts
@@ -0,0 +1,7 @@
+import { Page, Subpage } from "data/page";
+import { Project } from "data/project";
+
+
+export const getPageUrl = (page: Page) => `/${page.metadata.slug}`;
+export const getSubageUrl = (page: Page, subpage: Subpage) => `/${page.metadata.slug}/${subpage.metadata.slug}`;
+export const getProjectUrl = (project: Project) => `/projekt/${project.slug}`;
