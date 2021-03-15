99060ece0880e40e9e1f4ca37f9d645d8d6dc78d
Maximilian Schulke <hello@maximilianschulke.com>
2021-03-15 16:29:31 +0100

    add a project number formatting util

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,2 +1,5 @@
 export const phoneNumber = (nr: string = ''): string =>
-	`${nr.substring(0, 3)} ${nr.substring(3, 6)} ${nr.substring(6).match(/.{1,2}/g)?.join(' ') || ''}`
+	`${nr.substring(0, 3)} ${nr.substring(3, 6)} ${nr.substring(6).match(/.{1,2}/g)?.join(' ') || ''}`;
+
+export const projectNumber = (nr: number): string =>
+	`N° ${nr < 10 ? `0${nr}` : nr}`;
