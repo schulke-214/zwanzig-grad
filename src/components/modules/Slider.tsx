00eb30bc4743804fb0a54e5302af12b6a48cbaa9
Maximilian Schulke <hello@maximilianschulke.com>
2021-03-16 14:23:18 +0100

    refactor the slider to support the generic picture

--- src/components/modules/Slider.tsx
+++ src/components/modules/Slider.tsx
@@ -1,24 +1,44 @@
 import React, { FunctionComponent, useContext } from 'react'
 import Swiper from 'react-id-swiper';
-import styled, { ThemeContext } from 'styled-components';
+import styled, { css, ThemeContext } from 'styled-components';
 
 import { rem } from 'lib/polished';
-import { tablet } from 'lib/media';
+import { desktop, landscape, tablet } from 'lib/media';
 
 import ModuleContainer from 'components/generic/ModuleContainer';
+import Picture from 'components/generic/Picture';
+
+import { CMSResponsiveImage } from 'data/cms';
 
 
-const SliderItem = styled.picture`
-	&, img {
-		display: block;
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
+interface SliderItemProps {
+	className?: string;
+	dimension: {
+		width: number;
+		height: number;
+	}
+}
+
+const sliderItemDimensionMixin = (width: string): any => css`
+	height: ${width} !important;
+	max-width: calc((${width} / ${(props: any) => props.dimension.height}) * ${(props: any) => props.dimension.width}) !important;
+`;
+
+const SliderItem = styled.div<SliderItemProps>`
+	display: block;
+	${sliderItemDimensionMixin('80vw')}
+
+	${landscape} {
+		${sliderItemDimensionMixin('50vw')}
+	}
+
+	${desktop} {
+		${props => sliderItemDimensionMixin(rem(props.theme.layout.maxWidth / 2))}
 	}
 `;
 
 interface SliderProps {
-	images: any;
+	images: CMSResponsiveImage[];
 	showTitle: boolean;
 	title: string;
 	className?: string;
@@ -28,8 +48,7 @@ const Slider: FunctionComponent<SliderProps> = ({ className, images }) => {
 	const theme = useContext(ThemeContext);
 
 	const settings = {
-		slidesPerView: 1.5,
-		centeredSlides: true,
+		slidesPerView: 'auto' as any,
 		shouldSwiperUpdate: true,
 		slideToClickedSlide: true,
 		loop: true,
@@ -39,14 +58,17 @@ const Slider: FunctionComponent<SliderProps> = ({ className, images }) => {
 		}
 	};
 
+
 	return (
 		<ModuleContainer>
 			<div className={className}>
 				<Swiper {...settings}>
-					{images.map((image: any) => (
-						<SliderItem key={image.id}>
-							<source srcSet={image.responsive.srcSet} media={tablet.replace('@media ', '')} />
-							<img src={image.responsive.src} alt={image.description} />
+					{images.map(image => (
+						<SliderItem
+							key={image.contentful_id}
+							dimension={image.file.details.image}
+						>
+							<Picture {...image} isCropped isSlim />
 						</SliderItem>
 					))}
 				</Swiper>
